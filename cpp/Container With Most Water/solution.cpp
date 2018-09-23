fd61bd07762cace4e284c5a0cf348b0e2a70339e
magicstone2018 <41269777+magicstone2018@users.noreply.github.com>
2018-09-23 16:49:41 -0700

    Update solution.cpp

--- cpp/Container With Most Water/solution.cpp
+++ cpp/Container With Most Water/solution.cpp
@@ -1,1 +1,27 @@
-class Solution
+class Solution {
+public:
+  int maxArea(vector<int>& height) {
+    int result = 0;
+    int size = height.size();
+    int w1 = 0;
+    int w2 = size - 1;
+    while (w1 < w2) {
+      int w = w2 - w1;
+      int h = min(height[w1], height[w2]);
+      int area = w * h;
+      if (area > result) {
+        result = area;
+      }
+      if (height[w1] <= height[w2]) {
+        while (height[w1] <= h && w1 < w2) {
+          w1++;
+        }
+      } else {
+        while (height[w2] <= h && w1 < w2) {
+          w2--;
+        }
+      }
+    }
+    return result;
+  }
+};
